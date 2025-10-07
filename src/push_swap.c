1bcd040575581e84b965f3d440cb8e3106f3417d
Luca Barbosa <lucabento.luca@gmail.com>
2025-10-07 02:44:53 -0300

    Starting list and converting numbers

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,86 @@
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 21:26:37 by lbento            #+#    #+#             */
-/*   Updated: 2025/10/02 22:02:40 by lbento           ###   ########.fr       */
+/*   Updated: 2025/10/06 01:10:12 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+static t_list *creation_list(int argc, char **argv);
+void free_and_exit(t_list **stack_a, t_list **stack_b, int exit_num);
+static void check_num (t_list *stack_a, int nbr);
+
+int	main(int argc, char **argv)
+{
+	t_list *stack_a;
+	t_list *stack_b;
+
+	if (argc != 1)
+	{
+		stack_a = creation_list(argc, argv);
+		stack_b = NULL;
+	}
+	return (0);
+}
+
+static t_list *creation_list(int argc, char **argv)
+{
+	t_list *first_list;
+	t_list *list;
+	t_list *temp;
+	int i;
+	int num;
+
+	first_list = (t_list *)malloc(sizeof(t_list));
+	if (!first_list)
+		free_and_exit(NULL, NULL, 1);
+	i = 1;
+	while (i < argc)
+	{
+		list = (t_list *)malloc(sizeof(t_list));
+		num = ft_atoi(argv[i]);
+		check_num(&first_list, num);
+		list->number = num;
+		list->position = -1;
+		list->previous = temp;
+		temp->next = list;
+		temp = list;
+		i++;
+	}
+	return (first_list);
+}
+
+static void check_num (t_list *stack_a, int nbr)
+{
+	if (nbr >= 2147483648)
+		free_and_exit(stack_a, NULL, 1);
+	if (nbr < -2147483648)
+		free_and_exit(stack_a, NULL, 1);
+}
+
+
+void free_and_exit(t_list **stack_a, t_list **stack_b, int exit_num)
+{
+	t_list *list;
+	t_list *temp;
+
+	list = *stack_a;
+	while (list)
+	{
+		temp = list;
+		list = list->next;
+		free(temp);
+	}
+	list = *stack_b;
+	while (list)
+	{
+		temp = list;
+		list = list->next;
+		free(temp);
+	}
+	if(exit_num == 0)
+		exit(0);
+	if(exit_num == 1)
+		exit(1);
+}
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 22:02:31 by lbento            #+#    #+#             */
-/*   Updated: 2025/10/02 22:02:39 by lbento           ###   ########.fr       */
+/*   Updated: 2025/10/06 00:32:06 by lbento           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,14 @@
 # include <stdlib.h>
 # include "libft.h"
 
-
+typedef struct s_list
+{
+	int					position;
+	int					number;
+	struct s_list	*next;
+	struct s_list	*previous;
+	
+}	t_list;
 
 
 #endif
